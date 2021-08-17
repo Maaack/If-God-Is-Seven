690c82bfe19da03d9a3c80d0e92b62439a367d3d
Maaack <marek.belski@gmail.com>
2021-08-16 23:41:40 -0700

    COND-01: Merges toothbrush interaction events.

--- Scenes/GameUI/EventUI/BathroomEvents/ToothbrushEventUI.gd
+++ Scenes/GameUI/EventUI/BathroomEvents/ToothbrushEventUI.gd
@@ -1,7 +1,6 @@
 extends EventUI
 
-
 func _on_BrushTeethButton_pressed():
-	emit_signal("added_historical_note", "You brushed your teeth with YOUR TOOTHBRUSH.")
+	emit_signal("added_historical_note", "You brushed your teeth with your toothbrush.")
 	emit_signal("attempted_waiting", 2)
 	end_event()
