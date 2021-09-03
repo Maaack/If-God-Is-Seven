4bf77a0f1c7fde2a2ed34006c67ae87d3890cfd2
Maaack <marek.belski@gmail.com>
2021-09-03 12:42:58 -0700

    UI-03: Adds a way to skip the main menu animation.

--- Scenes/GameView/TitleScreenScene.gd
+++ Scenes/GameView/TitleScreenScene.gd
@@ -4,6 +4,8 @@ extends Control
 signal scene_changed(scene)
 
 var start_game_scene = preload("res://Scenes/GameUI/GameUIv3.tscn")
+var click_counter = 0
+var ff_clicks_threshold = 2
 
 func _on_CloseCreditsButton_pressed():
 	$Credits.hide()
@@ -16,3 +18,16 @@ func _on_QuitButton_pressed():
 
 func _on_StartButton_pressed():
 	emit_signal("scene_changed", start_game_scene)
+
+func _fast_forward_animations():
+	$BlackoutFilmBars/AnimationPlayer.seek(8)
+	$Panel/AnimationPlayer.seek(8)
+	$Panel2/AnimationPlayer.seek(8)
+	$VBoxContainer/AnimationPlayer.seek(8)
+
+func _on_MouseDetect_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			click_counter += 1
+			if click_counter >= ff_clicks_threshold:
+				_fast_forward_animations()
