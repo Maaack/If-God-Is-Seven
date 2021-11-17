81a81c93a6e8810ed032e9492a31e18c293d0a92
Maaack <marek.belski@gmail.com>
2021-11-16 22:57:04 -0800

    UI-04: Starts UI of character events.

---
+++ Scenes/GameUI/Events/CharacterEvents/CharacterEvent.gd
@@ -0,0 +1,24 @@
+extends BaseEventUI
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed() and event.button_index == BUTTON_LEFT:
+			advance_event()
+
+func _ready():
+	start_event()
+	$HBoxContainer/LeftPortraitContainer/LeftPortrait.texture = interactable_data.portrait
+	$HBoxContainer/RightPortraitContainer/RightPortrait.texture = interactable_data.portrait
+	match interaction_type:
+		InteractionConstants.interaction_types.SPEAK:
+			if interactable_data.dialogue != "":
+				for note in interactable_data.dialogue.split("\n"):
+					add_note(note)
+		InteractionConstants.interaction_types.LOOK:
+			if interactable_data.visual_description != "":
+				for note in interactable_data.visual_description.split("\n"):
+					add_note(note)
+
+func on_subtitle_finish_displaying():
+	end_event()
